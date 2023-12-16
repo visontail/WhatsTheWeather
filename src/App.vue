e989c56691b07ab0aa1565799f72f64fb7c7336b
visontail <the051425@gmail.com>
2023-12-16 20:56:24 +0100

    vue setup

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <WeatherApp/>
+</template>
+
+<script>
+import WeatherApp from './components/WeatherApp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    WeatherApp
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/WeatherApp.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="weather-app">
+    <h1 id="title">Weather App</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WeatherApp',
+}
+</script>
+
+<style scoped>
+#weather-app {
+  background-color: #161A30;
+  color: #F0ECE5
+}
+#title {
+  font-size: 3rem;
+  font-weight: 300;
+  margin: 0;
+  padding: 1rem;
+  text-align: center;
+}
+</style>
