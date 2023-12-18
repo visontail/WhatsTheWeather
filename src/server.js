4263460c8bdcfdde6331614b5e77fb4defcafe03
visontail <the051425@gmail.com>
2023-12-18 18:02:51 +0100

    .env, fetch data function

--- src/server.js
+++ src/server.js
@@ -1,21 +1,46 @@
 const axios = require('axios');
+const dotenv = require('dotenv');
+dotenv.config({path: '../.env'});
+
+const city = 'Fort-Walton-Beach';
 
 const options = {
   method: 'GET',
   url: 'https://weatherapi-com.p.rapidapi.com/current.json',
-  params: {q: '53.1,-0.13'},
+  params: {q: city},
   headers: {
-    'X-RapidAPI-Key': '',
+    'X-RapidAPI-Key': process.env.API_KEY,
     'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
   }
 };
 
-async function getWeather() {
+async function fetchWeatherData() {
     try {
         const response = await axios.request(options);
-        console.log(response.data);
+        const data = {
+          "location": [
+            {
+              "name": response.data.location.name,
+              "region": response.data.location.region,
+              "country": response.data.location.country,
+              "lat": response.data.location.lat,
+              "lon": response.data.location.lon,
+              "localtime": response.data.location.localtime
+            }
+          ],
+          "weather": [
+            {
+              "tempC": response.data.current.temp_c,
+              "tempF": response.data.current.temp_f,
+              "condition": response.data.current.condition.text,
+              "feelsLikeC": response.data.current.feelslike_c,
+              "feelsLikeF": response.data.current.feelslike_f
+            }
+          ],
+        }
+        console.log(data);
     } catch (error) {
         console.error(error);
     }
 }
-getWeather();
+fetchWeatherData();
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const path = require('path');
+const os = require('os');
+const crypto = require('crypto');
+const stream = require('stream');
+
+module.exports = {
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        path: require.resolve('path-browserify'),
+        os: require.resolve('os-browserify/browser'),
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+      },
+    },
+  },
+};
