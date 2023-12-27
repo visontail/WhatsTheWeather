6d2c45bc3dfb3771c74f7028394ef97946cb6551
visontail <the051425@gmail.com>
2023-12-27 16:19:22 +0100

    toogle btn done

--- src/components/DisplayWeather.vue
+++ src/components/DisplayWeather.vue
@@ -3,8 +3,8 @@
     <div id="weather-main" class="temperature" :style="{ backgroundImage: `url('${weatherData.current.condition.background_img}')` }">
       <h2>{{ weatherData.location.name }}, {{ weatherData.location.country }}</h2>
       <p>{{ weatherData.location.region }}</p>
-      <p id="temp">{{ weatherData.current.temp_c }}°C</p>
-      <p id="temp">{{ weatherData.current.temp_f }}°F</p>
+      <p v-if="isCelcius" id="temp">{{ weatherData.current.temp_c }}°C</p>
+      <p v-else id="temp">{{ weatherData.current.temp_f }}°F</p>
       <p>{{ weatherData.location.hour }}</p>
       <p>{{ weatherData.location.dayOfWeek }}</p>
       <p>{{ weatherData.location.welcomeText }}</p>
@@ -12,13 +12,14 @@
       <img id="condition-icon" :src="weatherData.current.condition.icon"/>
     </div>
     <div id="weather-details" class="weather-details">
+      <button id="toggle-unit" @click="toggleUnit">{{ isCelcius ? '°F' : '°C' }}</button>
       <h2>{{ weatherData.location.name }}</h2>
       <p>{{ weatherData.location.localtime }}</p>
       <p>{{ weatherData.location.country }}</p>
       <p>{{ weatherData.location.region }}</p>
       <p>Coordinates: {{ weatherData.location.lat }}, {{ weatherData.location.lon }}</p>
-      <p>Feels Like: {{ weatherData.current.feelslike_c }}°C / {{ weatherData.current.feelslike_f }}°F</p>
-      <p>Wind: {{ weatherData.current.wind_kph }} kph / {{ weatherData.current.wind_mph }} mph</p>
+      <p>Feels Like: {{ isCelcius ? weatherData.current.feelslike_c : weatherData.current.feelslike_f }}°{{ isCelcius ? 'C' : 'F' }}</p>
+      <p>Wind: {{ isCelcius ? weatherData.current.wind_kph : weatherData.current.wind_mph }} {{ isCelcius ? 'kph' : 'mph' }}</p>
       <p>Wind Direction: {{ weatherData.current.wind_dir }}</p>
       <p>Humidity: {{ weatherData.current.humidity }}%</p>
     </div>
@@ -31,9 +32,13 @@ export default {
   props: {
     weatherData: Object,
   },
+  data() {
+    return {
+      isCelcius: true,
+    };
+  },
   methods: {
-    isCelcius: true,
-    toggleTemp() {
+    toggleUnit() {
       this.isCelcius = !this.isCelcius;
     },
   }
